68496d11327a81fd315f0756b19d41b0caf23dc7
rahathossenantor <md.rahathossenantor@gmail.com>
2024-04-13 20:49:32 +0600

    Lecture 1 completed

---
+++ learn_py.py
@@ -0,0 +1,29 @@
+# first ans
+n1 = int(input("Num 1: "))
+n2 = int(input("Num 2: "))
+sum = n1 + n2
+print(sum)
+
+# second ans
+side = float(input("Enter square side : "))
+side *= side
+print(side)
+
+# thirtd ans avrg of two nums
+n1 = float(input("Enter number 1 : "))
+n2 = float(input("Enter number 2 : "))
+result = (n1 + n2) / 2
+print(result)
+
+# fourth ans
+a = int(input("Enter num 1 : "))
+b = int(input("Enter num 2 : "))
+print(a>b)
+
+# fifth ans
+first_name = input("Enter your first name : ")
+print("The length of your first name is", len(first_name))
+
+# sixth ans
+str = "Hi! I am The $ Sign, This is $ $99.99"
+print("Total count of $ is", str.count("$"))
